7d1a006171fc76fe2ab105bfd4b7f000ba320698
TomBrnr <thomabrunner@tsn.at>
2025-05-06 14:56:52 +0200

    Teams

---
+++ WattCount/Models/Teams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WattCount.Models
+{
+    class Teams
+    {
+        public int TeamId { get; set; }
+        public String NamePlayer1 { get; set; }
+        public String NamePlayer2 { get; set; }
+        public int Bummerlen { get; set; }
+    }
+}
