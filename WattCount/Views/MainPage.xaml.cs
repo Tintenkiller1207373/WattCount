bc1bbcc31ccf7ee5d928cc969e5ea7ce503cc604
TomBrnr <thomabrunner@tsn.at>
2025-05-06 14:40:16 +0200

    weck

--- WattCount/Views/MainPage.xaml.cs
+++ WattCount/Views/MainPage.xaml.cs
@@ -3,8 +3,7 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
-        int count2;
-        int count3;
+       
         public MainPage()
         {
             InitializeComponent();
