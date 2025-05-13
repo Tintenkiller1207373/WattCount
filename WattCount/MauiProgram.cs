ddabc0f7a1f574865db4f3de0d0db8c6720b247d
Fabian Klingenschmid <fabian.klingenschmid@icloud.com>
2025-05-13 13:35:29 +0200

    Kleinigkeiten

--- WattCount/MauiProgram.cs
+++ WattCount/MauiProgram.cs
@@ -1,29 +1,24 @@
 ﻿using Microsoft.Extensions.Logging;
 using WattCount.ViewModels;
 using WattCount.Views;
+using CommunityToolkit.Maui;
 
 namespace WattCount;
-
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        }).UseMauiCommunityToolkit();
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
-
         builder.Services.AddTransient<MainPage>();
         builder.Services.AddTransient<MainPageViewModel>();
-
         return builder.Build();
-	}
+    }
 }
