8892a53340216de98af504c78a1814224f2368ac
Fabian Klingenschmid <fabian.klingenschmid@icloud.com>
2025-05-13 14:40:45 +0200

    Commands

--- WattCount/ViewModels/SpielstandViewModel.cs
+++ WattCount/ViewModels/SpielstandViewModel.cs
@@ -18,55 +18,128 @@ namespace WattCount.ViewModels
         [ObservableProperty]
         int[] _teamB = new int[6];
 
+
         // Team 1 Punkte
         [RelayCommand]
         public async Task AddTwoA() {
+            for (int i = 0; i < TeamA.Length; i++) {
+                if (TeamA[i] == 0) {
+                    TeamA[i] = 2;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddThreeA() {
+            for (int i = 0; i < TeamA.Length; i++) {
+                if (TeamA[i] == 0) {
+                    TeamA[i] = 3;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddFourA() {
+            for (int i = 0; i < TeamA.Length; i++) {
+                if (TeamA[i] == 0) {
+                    TeamA[i] = 4;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddFiveA() {
+            for (int i = 0; i < TeamA.Length; i++) {
+                if (TeamA[i] == 0) {
+                    TeamA[i] = 5;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddSixA() {
+            for (int i = 0; i < TeamA.Length; i++) {
+                if (TeamA[i] == 0) {
+                    TeamA[i] = 6;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddSevenA() {
+            for (int i = 0; i < TeamA.Length; i++) {
+                if (TeamA[i] == 0) {
+                    TeamA[i] = 7;
+                    break;
+                }
+            }
         }
 
 
         // Team 2 Punkte
         [RelayCommand]
         public async Task AddTwoB() {
+            for (int i = 0; i < TeamB.Length; i++) {
+                if (TeamB[i] == 0) {
+                    TeamB[i] = 2;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddThreeB() {
+            for (int i = 0; i < TeamB.Length; i++) {
+                if (TeamB[i] == 0) {
+                    TeamB[i] = 3;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddFourB() {
+            for (int i = 0; i < TeamB.Length; i++) {
+                if (TeamB[i] == 0) {
+                    TeamB[i] = 4;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddFiveB() {
+            for (int i = 0; i < TeamB.Length; i++) {
+                if (TeamB[i] == 0) {
+                    TeamB[i] = 5;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddSixB() {
+            for (int i = 0; i < TeamB.Length; i++) {
+                if (TeamB[i] == 0) {
+                    TeamB[i] = 6;
+                    break;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task AddSevenB() {
+            for (int i = 0; i < TeamB.Length; i++) {
+                if (TeamB[i] == 0) {
+                    TeamB[i] = 7;
+                    break;
+                }
+            }
         }
 
 
