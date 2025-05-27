f5b86b7e4a4b12b23a15b2adf6cddcf365176537
TomBrnr <thomabrunner@tsn.at>
2025-05-27 13:55:25 +0200

    DbManagerKlasse

---
+++ WattCount/Models/DbManagerSQLite.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WattCount.Models;
+
+namespace MauiBasics.Services {
+
+    
+    public class DbManagerSQLite : DbContext {
+       
+        public DbSet<Teams> Teams { get; set; }
+
+        public DbManagerSQLite() {
+            SQLitePCL.Batteries_V2.Init();
+
+            this.Database.EnsureCreated();
+        }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "app_db_local.sqlite");
+
+            // C:\Users\Thomas\AppData\Local\User Name\com.companyname.mauibasics\Data\app_db_local.sqlite
+
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+
+    }
+
+}
