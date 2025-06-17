a5266dccd304ca379122c18c15f89136f5669d7d
Fabian Klingenschmid <fabian.klingenschmid@icloud.com>
2025-06-17 13:56:24 +0200

    Viwmodel von SelectTeam Page

--- WattCount/ViewModels/SelectTeamViewModel.cs
+++ WattCount/ViewModels/SelectTeamViewModel.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiBasics.Services;
+using WattCount.Models;
 
 namespace WattCount.ViewModels
 {
-    public class SelectTeamViewModel
+    public partial class SelectTeamViewModel : ObservableObject
     {
+
+        [ObservableProperty]
+        private string _name;
+
+        private readonly DbManagerSQLite _dbManagerSQLite;
+
+        public SelectTeamViewModel(DbManagerSQLite dbManagerSQLite) {
+            this._dbManagerSQLite = dbManagerSQLite;
+        }
+
+
+        [RelayCommand]
+        public async Task Send() {
+            Teams team = new() {
+                TeamsId = 0,
+                TeamName = this.Name,
+            };
+            this._dbManagerSQLite.Teams.Add(team);
+            await this._dbManagerSQLite.SaveChangesAsync();
+
+
+        }
+
     }
 }
