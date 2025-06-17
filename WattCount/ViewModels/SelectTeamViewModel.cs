6970f24c1dc36e9c4724056d09892c6a993cd30b
Fabian Klingenschmid <fabian.klingenschmid@icloud.com>
2025-06-17 14:58:23 +0200

    SpielstandPage noch nicht Fertig!

--- WattCount/ViewModels/SelectTeamViewModel.cs
+++ WattCount/ViewModels/SelectTeamViewModel.cs
@@ -1,27 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MauiBasics.Services;
+using Microsoft.EntityFrameworkCore;
 using WattCount.Models;
 
 namespace WattCount.ViewModels
 {
-    public partial class SelectTeamViewModel : ObservableObject
-    {
+    public partial class SelectTeamViewModel : ObservableObject {
+
+        public SelectTeamViewModel() {
+            LoadTeams();
+        }
+
 
         [ObservableProperty]
         private string _name;
+
+        [ObservableProperty]
+        ObservableCollection<Teams> _teams = new();
 
         private readonly DbManagerSQLite _dbManagerSQLite;
 
         public SelectTeamViewModel(DbManagerSQLite dbManagerSQLite) {
             this._dbManagerSQLite = dbManagerSQLite;
         }
-
 
         [RelayCommand]
         public async Task Send() {
@@ -31,9 +39,12 @@ namespace WattCount.ViewModels
             };
             this._dbManagerSQLite.Teams.Add(team);
             await this._dbManagerSQLite.SaveChangesAsync();
-
-
         }
 
+
+        // Methode um die Teams in die ObservableColletion zu laden
+        public async void LoadTeams() {
+          Teams = new ObservableCollection<Teams>(await _dbManagerSQLite.Teams.ToListAsync());
+        }
     }
 }
